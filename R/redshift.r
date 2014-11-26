003696bc35e3e0809aedf6f05a9af205dd98d812
Eeshan Chatterjee <eeshanchatterjee@gmail.com>
2014-11-26 16:13:52 +0530

    added unload and sendquery functions

--- R/redshift.r
+++ R/redshift.r
@@ -33,3 +33,24 @@ redshift.columns <- function(conn, tableName) {
 redshift.query <- function(conn, ...) {
   dbGetQuery(conn, paste(..., collapse=' ', sep=' '))
 }
+
+redshift.submitquery <- function(conn, ...) {
+  dbSendUpdate(conn, paste(..., collapse=' ', sep=' '))
+}
+
+redshift.unload <- function(conn, query, filename, aws.accesskey, aws.secretkey, delim = ',', 
+                            allowOverwrite = TRUE, parallel = TRUE, zip = TRUE){
+  query = gsub("'","''",query)
+  sql = paste0("('",gsub(";","",query),"')")
+  loc = paste0("'",filename,"'")
+  cred = paste0("'aws_access_key_id=",aws.accesskey,";aws_secret_access_key=",aws.secretkey,"'")
+  delimiter = paste0("'",delim,"'")
+  if(!parallel) par = "PARALLEL OFF" else par = ""
+  if(!zip) gzip = "" else gzip = "GZIP"
+  if(!allowOverwrite) aow = "" else aow = "ALLOWOVERWRITE"
+  
+  unload.query = paste(c("UNLOAD",sql, "TO", loc, "CREDENTIALS", cred, "DELIMITER", delimiter, gzip, par, aow,";"),collapse = " ")
+  unload.query = gsub("\\s+"," ",unload.query)
+  print(unload.query)
+  dbSendUpdate(conn,unload.query)
+}
